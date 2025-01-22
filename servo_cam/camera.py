dbfad89715f0c289c2072169004b6316e76e2385
Oleksandr Valentirov <aleks0010v@gmail.com>
2025-01-22 20:22:17 +0200

    adding drawing callback for the future objects detection

--- servo_cam/camera.py
+++ servo_cam/camera.py
@@ -3,8 +3,9 @@
 import time
 import socket
 import signal
-from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder, MJPEGEncoder
+import cv2
+from picamera2 import Picamera2, MappedArray
+from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
 from argparse import ArgumentParser
 
@@ -13,10 +14,21 @@ is_exit = False
 MAX_PACKET_SIZE = 1024
 
 
+def drawing(request):
+    with MappedArray(request, "main") as m:
+        # draw a green rectangle example
+        # cv2.rectangle(m.array, (100, 100), (500, 500), (0, 255, 0, 0))
+        # draw a red cross example
+        # cv2.line(m.array, (945, 540), (975, 540), (255, 0, 0), 1)
+        # cv2.line(m.array, (960, 525), (960, 555), (255, 0, 0), 1)
+        pass
+
+
 def init_cam():
     picam2 = Picamera2(0)
-    config = picam2.create_video_configuration()
+    config = picam2.create_video_configuration(main={"size": (1920, 1080)})
     picam2.configure(config)
+    picam2.post_callback = drawing
     picam2.start(show_preview=False)
     picam2.set_controls({"ScalerCrop": (768, 432, 3072, 1728)})
     picam2.capture_metadata()  # sync
@@ -46,7 +58,7 @@ if __name__ == "__main__":
     cam.start_recording(encoder, FileOutput(stream))
 
     while not is_exit:  # wait for the sigint
-        pass
+        pass  # run a tensorflow here
 
     cam.stop_recording()
     sock.close()
